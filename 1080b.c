586c2bfca421e55cf406fe19a99d1f7e31d7dd8d
FABRICIO RIBEIRO UCHIDA <fabricio.uchida@unifesp.br>
2026-09-21 22:26:14 -0300

    change 1080.b to 1080b.c
    Add header comments with problem details and objectives.

---
+++ 1080b.c
@@ -0,0 +1,10 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Algortimo e Estrutura de Dados 2026S1
+Nome        : <<<Fabrício Ribeiro Uchida>>>
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1080
+Data        : 31/08/2026
+Objetivo    : Identificar maior e valor e sua posição entre 100 entradas com aloc. dinâmica
+Dificuldade : Fazer a alocação dinamica, sintaxe
+Uso de IA   : Não foi utilizado
+-------------------------------------------------------------------------- */
